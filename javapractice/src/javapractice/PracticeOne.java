c395cbc85a52ffd1e2d6cb9767fb462c52be611c
skeletoro <69654563+skeletoro@users.noreply.github.com>
2021-05-14 07:44:51 -0700

    zh pushing java practice

---
+++ javapractice/src/javapractice/PracticeOne.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+public class PracticeOne {
+
+	public static void main(String[] args) {
+		
+		int num = 17;
+System.out.println("hello world");
+String name = "sally smith";
+System.out.println(name);
+System.out.println(num);
+System.out.println("hello world");
+System.out.println("hello world");
+System.out.print("hello world" + " " + "hello world");
+
+
+	}
+
+}
