96a3cae432470b6f79a919a153f130bfbde9051d
skeletoro <69654563+skeletoro@users.noreply.github.com>
2021-05-18 06:34:30 -0700

    zh pushing java practices

---
+++ javapractices/src/javapractices7/ArrayPractice.java
@@ -0,0 +1,21 @@
+package javapractices7;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		int[] oneToTwenty = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		//for (int x = 0; x<=oneToTwenty.length; x++ ) {
+			//System.out.println(x);
+		int[] numbers = new int[11];
+		for (int i = 0; i<=10; i++) {
+			numbers[i] = i;
+			int result = i * 10;
+			 System.out.println(result);
+		}
+	
+			
+		}
+		}
+	//}
+
+
